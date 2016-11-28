fd7a9320ee5008c6d1e2ac265fc02014cdf7dc37
Jim Liu <jimliu741523@gmail.com>
2016-11-28 07:20:59 +0000

    version 0.2

--- R/CTM.R
+++ R/CTM.R
@@ -1,71 +1,94 @@
 #' @title Term Document Matrix
 #' @description Constructs Term-Document Matrix from Chinese Text Documents.
 #'
-#' @param doc The Chinese text document.
-#' @param weighting Available weighting function with matrix are tf, tfidf.
+#' @param doc The Chinese text document. A vector of Chinese strings.
+#' @param weighting Available weighting function with matrix are binary, count, tf, tfidf. See details.
+#' @param EngTermDeleted remove English from text documents.
+#' @param NumTermDeleted remove Numbers from text documents.
 #' @param shortTermDeleted Deltected short word when nchar <2.
-#' @author Jim Liu
+#' @author Jim Liu,  Quan Gu
 #' @details
 #' This function run a Chinese word segmentation by jiebeR and build
-#' term-document matrix, and there is two weighting function with matrix,
-#' term frequency and term frequency inverse document frequency.
+#' term-document matrix, and there is four weighting function with matrix, and
+#' "binary" means value can only be 1 if the term occurs, "count" means how many times the term occurs in a doc,
+#'  "tf" means term frequency and "tfidf" means term frequency inverse document frequency.
 #'
 #'
 #' @export
 #' @import jiebaR
+#' @import plyr
 #' @examples
 #' library(CTM)
-#' a <- "hello taiwan"
-#' b <- "world of tank"
-#' c <- "taiwan weather"
-#' d <- "local weather"
-#' text <- t(data.frame(a,b,c,d))
-#' tdm <- CTDM(doc = text, weighting = "tfidf", shortTermDeleted = FALSE)
+#' a1 <- "hello taiwan"
+#' b1 <- "world of tank"
+#' c1 <- "taiwan weather"
+#' d1 <- "local weather"
+#' text1 <- t(data.frame(a1,b1,c1,d1))
+#' tdm1 <- CTDM(doc = text1, weighting = "tfidf", EngTermDeleted = FALSE, shortTermDeleted = FALSE)
 
 
 
-CTDM <- function(doc,weighting,shortTermDeleted){
+CTDM <- function(doc,weighting,EngTermDeleted = TRUE,NumTermDeleted = TRUE, shortTermDeleted = TRUE){
   ###jiebaR
-  cutter <- worker()
-  cutfunc <- function(s){
-    return(cutter <= s)
-  }
-  dataText <- doc
-  segMatrix <- matrix(nrow = 1,ncol = 2)
-  colnames(segMatrix) <- c("id","term")
-  for(i in 1:length(dataText)){
-    uniSeg <- data.frame(i,as.matrix(cutter[dataText[i]]))
-    colnames(uniSeg) <- c("id","term")
-    segMatrix <- rbind(segMatrix,uniSeg)
-  }
-  segMatrix <- segMatrix[-1,]
-  ###
-  term <- unique(segMatrix[,2])
+  cutter <- jiebaR::worker()
+  dataText <- as.matrix(doc)
+  if(EngTermDeleted){dataText <- gsub(pattern ="[A-Za-z]" ,replacement = "",x = dataText)}
+  if(NumTermDeleted){dataText <- gsub(pattern ="[0-9]" ,replacement = "",x = dataText)}
+
+  segs <- sapply(dataText,USE.NAMES = F,FUN = function(x){
+    temp=(cutter <= x)
+    temp[nchar(temp)>0]
+  })
+
+  allTerms=unlist(segs)
+  lens=sapply(segs,length)                               #count of terms in different id
+  allIds=rep(1:length(segs),lens)
+  segMatrix <- data.frame(id=allIds,term=allTerms,stringsAsFactors = F)
+  ###plyr
+  it_freq <- plyr::count(segMatrix,vars = c("id","term"))#count of different terms in each id, "it" means id_terms
+
+  term <- unique(segMatrix$term)
   if(shortTermDeleted){
-    term <- term[-which(nchar(term)<2)]
+    term <- term[nchar(term)>=2]
   }
-  dtm <- matrix(nrow = length(dataText),ncol = length(term),0)
+
+  dtm <- matrix(nrow = length(dataText),ncol = length(term),0)#likely to be sparse matrix, can use Matrix::Matrix
+
+  if(weighting=="binary"){
+    for(i in 1:length(term)){
+      find1 <- it_freq$id[it_freq$term==term[i]]
+      dtm[find1,i] <- 1
+    }
+  }
+
+  if(weighting=="count"){
+    for(i in 1:length(term)){
+      find1 <- it_freq[it_freq$term==term[i],c("id","freq")]
+      dtm[find1$id,i] <- find1$freq
+    }
+  }
+
   if(weighting=="tf"){
     for(i in 1:length(term)){
-      find1 <- table(segMatrix[which(segMatrix[,2]==term[i]),1])
-      find2 <- table(segMatrix[which(segMatrix[,1] %in% as.numeric(names(find1))),1])
-      dtm[as.numeric(names(find1)),i] <- find1/find2
-      cat("\r","Calculating for",i,"of",length(term),"term")
+      find1 <- it_freq[it_freq$term==term[i],c("id","freq")]
+      find2 <- lens[find1$id]
+      dtm[find1$id,i] <- find1$freq/find2
     }
   }
 
   if(weighting=="tfidf"){
+    n_row=nrow(dtm)
     for(i in 1:length(term)){
-      find1 <- table(segMatrix[which(segMatrix[,2]==term[i]),1])
-      find2 <- table(segMatrix[which(segMatrix[,1] %in% as.numeric(names(find1))),1])
-      dtm[as.numeric(names(find1)),i] <- find1/find2
-      dtm[,i] <- dtm[,i]*log(nrow(dtm)/(1+length(which(dtm[,i]>0))))
-      cat("\r","Calculating for",i,"of",length(term),"term")
+      find1 <- it_freq[it_freq$term==term[i],c("id","freq")]
+      find2 <- lens[find1$id]
+      dtm[find1$id,i] <- find1$freq/find2
+      dtm[,i] <- dtm[,i]*log(n_row/(1+nrow(find1)))
     }
   }
   colnames(dtm) <- term
-  rownames(dtm) <- 1:length(dataText)
-  dtm <- t(dtm)
+  rownames(dtm) <- paste("doc",1:length(doc))
+  tdm=t(dtm)
+  tdm
 }
 
 
@@ -74,72 +97,95 @@ CTDM <- function(doc,weighting,shortTermDeleted){
 #' @title Document Term Matrix
 #' @description Constructs Document-Term Matrix from Chinese Text Documents.
 #'
-#' @param doc The Chinese text document.
-#' @param weighting Available weighting function with matrix are tf, tfidf.
+#' @param doc The Chinese text document. A vector of Chinese strings.
+#' @param weighting Available weighting function with matrix are binary, count, tf, tfidf. See details.
+#' @param EngTermDeleted remove English from text documents.
+#' @param NumTermDeleted remove Numbers from text documents.
 #' @param shortTermDeleted Deltected short word when nchar <2.
-#' @author Jim Liu
+#' @author Jim Liu,  Quan Gu
 #' @details
-#' This function run a Chinese word segmentation by jiebeR and build document-term matrix
-#' and there is two weighting function with matrix,
-#' term frequency and term frequency inverse document frequency.
+#' This function run a Chinese word segmentation by jiebeR and build
+#' document-term matrix, and there is four weighting function with matrix, and
+#' "binary" means value can only be 1 if the term occurs, "count" means how many times the term occurs in a doc,
+#'  "tf" means term frequency and "tfidf" means term frequency inverse document frequency.
 #'
 #'
 #' @export
 #' @import jiebaR
+#' @import plyr
 #' @examples
 #' library(CTM)
-#' a <- "hello taiwan"
-#' b <- "world of tank"
-#' c <- "taiwan weather"
-#' d <- "local weather"
-#' text <- t(data.frame(a,b,c,d))
-#' dtm <- CDTM(doc = text, weighting = "tfidf", shortTermDeleted = FALSE)
+#' a1 <- "hello taiwan"
+#' b1 <- "world of tank"
+#' c1 <- "taiwan weather"
+#' d1 <- "local weather"
+#' text1 <- t(data.frame(a1,b1,c1,d1))
+#' dtm1 <- CTDM(doc = text1, weighting = "tfidf",EngTermDeleted = FALSE, shortTermDeleted = FALSE)
 
 
-
-CDTM <- function(doc,weighting,shortTermDeleted){
+CDTM <- function(doc,weighting,EngTermDeleted = TRUE, NumTermDeleted = TRUE, shortTermDeleted =TRUE){
   ###jiebaR
-  cutter <- worker()
-  cutfunc <- function(s){
-    return(cutter <= s)
-  }
-  dataText <- doc
-  segMatrix <- matrix(nrow = 1,ncol = 2)
-  colnames(segMatrix) <- c("id","term")
-  for(i in 1:length(dataText)){
-    uniSeg <- data.frame(i,as.matrix(cutter[dataText[i]]))
-    colnames(uniSeg) <- c("id","term")
-    segMatrix <- rbind(segMatrix,uniSeg)
-  }
-  segMatrix <- segMatrix[-1,]
-  ###
-  term <- unique(segMatrix[,2])
+  cutter <- jiebaR::worker()
+  dataText <- as.matrix(doc)
+  if(EngTermDeleted){dataText <- gsub(pattern ="[A-Za-z]" ,replacement = "",x = dataText)}
+  if(NumTermDeleted){dataText <- gsub(pattern ="[0-9]" ,replacement = "",x = dataText)}
+
+  segs <- sapply(dataText,USE.NAMES = F,FUN = function(x){
+    temp=(cutter <= x)
+    temp[nchar(temp)>0]
+  })
+
+  allTerms=unlist(segs)
+  lens=sapply(segs,length)                               #count of terms in different id
+  allIds=rep(1:length(segs),lens)
+  segMatrix <- data.frame(id=allIds,term=allTerms,stringsAsFactors = F)
+  ###plyr
+  it_freq <- plyr::count(segMatrix,vars = c("id","term"))#count of different terms in each id, "it" means id_terms
+
+  term <- unique(segMatrix$term)
   if(shortTermDeleted){
-    term <- term[-which(nchar(term)<2)]
+    term <- term[nchar(term)>=2]
   }
-  dtm <- matrix(nrow = length(dataText),ncol = length(term),0)
+
+  dtm <- matrix(nrow = length(dataText),ncol = length(term),0)#likely to be sparse matrix, can use Matrix::Matrix
+
+  if(weighting=="binary"){
+    for(i in 1:length(term)){
+      find1 <- it_freq$id[it_freq$term==term[i]]
+      dtm[find1,i] <- 1
+    }
+  }
+
+  if(weighting=="count"){
+    for(i in 1:length(term)){
+      find1 <- it_freq[it_freq$term==term[i],c("id","freq")]
+      dtm[find1$id,i] <- find1$freq
+    }
+  }
+
   if(weighting=="tf"){
     for(i in 1:length(term)){
-      find1 <- table(segMatrix[which(segMatrix[,2]==term[i]),1])
-      find2 <- table(segMatrix[which(segMatrix[,1] %in% as.numeric(names(find1))),1])
-      dtm[as.numeric(names(find1)),i] <- find1/find2
-      cat("\r","Calculating for",i,"of",length(term),"term")
+      find1 <- it_freq[it_freq$term==term[i],c("id","freq")]
+      find2 <- lens[find1$id]
+      dtm[find1$id,i] <- find1$freq/find2
     }
   }
 
   if(weighting=="tfidf"){
+    n_row=nrow(dtm)
     for(i in 1:length(term)){
-      find1 <- table(segMatrix[which(segMatrix[,2]==term[i]),1])
-      find2 <- table(segMatrix[which(segMatrix[,1] %in% as.numeric(names(find1))),1])
-      dtm[as.numeric(names(find1)),i] <- find1/find2
-      dtm[,i] <- dtm[,i]*log(nrow(dtm)/(1+length(which(dtm[,i]>0))))
-      cat("\r","Calculating for",i,"of",length(term),"term")
+      find1 <- it_freq[it_freq$term==term[i],c("id","freq")]
+      find2 <- lens[find1$id]
+      dtm[find1$id,i] <- find1$freq/find2
+      dtm[,i] <- dtm[,i]*log(n_row/(1+nrow(find1)))
     }
   }
   colnames(dtm) <- term
-  rownames(dtm) <- 1:length(dataText)
-  dtm <- dtm
+  rownames(dtm) <- paste("doc",1:length(doc))
+  dtm
 }
+
+
 
 
 
@@ -147,39 +193,41 @@ CDTM <- function(doc,weighting,shortTermDeleted){
 #' @description Computing term count from text documents
 #'
 #' @param doc The Chinese text document.
+#' @param EngTermDeleted remove English from text documents.
+#' @param NumTermDeleted remove Numbers from text documents.
 #' @param shortTermDeleted Deltected short word when nchar <2.
 #' @author Jim Liu
 #' @details
 #' This function run a Chinese word segmentation by jiebeR and
-#' compute term count from these text document.
+#' compute term count from all these text document.
 #'
 #'
 #' @export
 #' @import jiebaR
 #' @examples
 #' library(CTM)
-#' a <- "hello taiwan"
-#' b <- "world of tank"
-#' c <- "taiwan weather"
-#' d <- "local weather"
-#' text <- t(data.frame(a,b,c,d))
-#' count <- termCount(doc = text, shortTermDeleted = FALSE)
+#' a1 <- "hello taiwan"
+#' b1 <- "world of tank"
+#' c1 <- "taiwan weather"
+#' d1 <- "local weather"
+#' text1 <- t(data.frame(a1,b1,c1,d1))
+#' count1 <- termCount(doc = text1, EngTermDeleted=FALSE, shortTermDeleted = FALSE)
 
 
-
-termCount <- function(doc,shortTermDeleted){
+termCount <- function(doc,EngTermDeleted=TRUE,NumTermDeleted=TRUE,shortTermDeleted=TRUE){
   ###jiebaR
-  cutter <- worker()
+  cutter <- jiebaR::worker()
   cutfunc <- function(s){
     return(cutter <= s)
   }
-  dataText <- doc
+  dataText <- as.matrix(doc)
+  if(EngTermDeleted){dataText <- gsub(pattern ="[A-Za-z]" ,replacement = "",x = dataText)}
+  if(NumTermDeleted){dataText <- gsub(pattern ="[0-9]" ,replacement = "",x = dataText)}
   res <- data.frame(table(unlist(lapply(dataText, cutfunc))))
   res[,1] <- as.character(res[,1])
   if(shortTermDeleted){
-    res <- res[which(nchar(res[,1])>1),]
+    res <- res[nchar(res[,1])>1,]
   }
   res <- res[order(-res$Freq),]
+  res
 }
-
-
